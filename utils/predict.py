c05edbe7ad8ce5e03b356ce09945214f43bc2a7c
PJarb <w.pachinee@gmail.com>
2026-01-22 01:47:46 +0700

    Refactor model loading and prediction functions

--- utils/predict.py
+++ utils/predict.py
@@ -1,15 +1,20 @@
+from pathlib import Path
 import joblib
 from sentence_transformers import SentenceTransformer
 
-def load_model():
-    clf = joblib.load("model/clf.pkl")
-    s2v_model = SentenceTransformer("model/s2v_model")
-    return clf, s2v_model
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_DIR = BASE_DIR / "model"
 
-def predict_label(texts, clf, s2v_model):
+def load_model():
+    logistic_model = joblib.load(MODEL_DIR / "logistic_model.pkl")
+    s2v_model = SentenceTransformer(str(MODEL_DIR / "sentence2vec_model"))
+    return logistic_model, s2v_model
+
+
+def predict_label(texts, logistic_model, s2v_model):
     embeddings = s2v_model.encode(
         list(texts),
         convert_to_numpy=True
     )
-    preds = clf.predict(embeddings)
+    preds = logistic_model.predict(embeddings)
     return ["Clear" if p == 1 else "Unclear" for p in preds]
