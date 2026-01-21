abf99b719d03973af9529c4d374983ca8048a4c1
PJarb <w.pachinee@gmail.com>
2026-01-22 03:56:33 +0700

    Add model loading and prediction functions

--- utils/predict.py
+++ utils/predict.py
@@ -2,9 +2,15 @@ from pathlib import Path
 import joblib
 from sentence_transformers import SentenceTransformer
 
+# ---------------------------
+# Path setup
+# ---------------------------
 BASE_DIR = Path(__file__).resolve().parents[1]
 MODEL_DIR = BASE_DIR / "model"
 
+# ---------------------------
+# Load models
+# ---------------------------
 def load_model():
     logistic_model = joblib.load(MODEL_DIR / "logistic_model.pkl")
 
@@ -13,3 +19,15 @@ def load_model():
     )
 
     return logistic_model, s2v_model
+
+
+# ---------------------------
+# Predict
+# ---------------------------
+def predict_label(texts, logistic_model, s2v_model):
+    embeddings = s2v_model.encode(
+        texts,
+        convert_to_numpy=True
+    )
+    preds = logistic_model.predict(embeddings)
+    return ["Clear" if p == 1 else "Unclear" for p in preds]
