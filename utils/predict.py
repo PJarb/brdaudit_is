4dd36298086c30885412df61642c15e1e9e00583
PJarb <w.pachinee@gmail.com>
2026-01-22 02:03:36 +0700

    Refactor model loading in predict.py
    Updated model loading to use Hugging Face's sentence2vec.

--- utils/predict.py
+++ utils/predict.py
@@ -6,15 +6,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MODEL_DIR = BASE_DIR / "model"
 
 def load_model():
+    # โหลด logistic model จาก GitHub
     logistic_model = joblib.load(MODEL_DIR / "logistic_model.pkl")
-    s2v_model = SentenceTransformer(str(MODEL_DIR / "sentence2vec_model"))
-    return logistic_model, s2v_model
 
-
-def predict_label(texts, logistic_model, s2v_model):
-    embeddings = s2v_model.encode(
-        list(texts),
-        convert_to_numpy=True
+    # โหลด sentence2vec จาก Hugging Face
+    s2v_model = SentenceTransformer(
+        "USERNAME/sentence2vec-brd"
     )
-    preds = logistic_model.predict(embeddings)
-    return ["Clear" if p == 1 else "Unclear" for p in preds]
+
+    return logistic_model, s2v_model
