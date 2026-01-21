d4b367cf704993d77fc273271f38f554664a2dd4
PJarb <w.pachinee@gmail.com>
2026-01-22 03:46:49 +0700

    Update predict.py

--- utils/predict.py
+++ utils/predict.py
@@ -6,21 +6,10 @@ BASE_DIR = Path(__file__).resolve().parents[1]
 MODEL_DIR = BASE_DIR / "model"
 
 def load_model():
-    # โหลด Logistic Regression (จาก GitHub)
     logistic_model = joblib.load(MODEL_DIR / "logistic_model.pkl")
 
-    # โหลด Sentence2Vec จาก Hugging Face
     s2v_model = SentenceTransformer(
         "Pachinee/sentence2vec-brd"
     )
 
     return logistic_model, s2v_model
-
-
-def predict_label(texts, logistic_model, s2v_model):
-    embeddings = s2v_model.encode(
-        texts,
-        convert_to_numpy=True
-    )
-    preds = logistic_model.predict(embeddings)
-    return ["Clear" if p == 1 else "Unclear" for p in preds]
